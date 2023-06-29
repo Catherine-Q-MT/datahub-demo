fc33db5dd1770a60b7c1979b1e3820b42656dab0
Catherine Quilley <catherine.quilley@madetech.com>
2023-06-29 18:30:25 +0100

    wip

--- lineage/manual.py
+++ lineage/manual.py
@@ -5,14 +5,14 @@ from datahub.emitter.rest_emitter import DatahubRestEmitter
 
 lineage_mce = builder.make_lineage_mce(
     [
-        builder.make_dataset_urn("s3", "datahub-cq/raw_data/hydropower-consumption.csv"),  # Upstream
-        builder.make_dataset_urn("s3", "datahub-cq/raw_data/solar-energy-consumption.csv"),  # Upstream
+        builder.make_dataset_urn("s3", "datahub-cq/raw_data/hydropower-consumption.csv"),  # Upstream connection or connections ( I am specifying the raw data in my pipeline)
+        builder.make_dataset_urn("s3", "datahub-cq/raw_data/solar-energy-consumption.csv"),  
 
     ],
     builder.make_dataset_urn("s3", "uk_solar_and_hydro.csv"),  # Downstream
 )
 
-# Create an emitter to the GMS REST API.
+# Create an emitter to the GMS REST API. Emitters can emit information and info can be accessed via API
 emitter = DatahubRestEmitter("http://localhost:8080")
 
 # Emit metadata!
